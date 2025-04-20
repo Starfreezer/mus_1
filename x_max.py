e5afbbc356af87f05606c7e272e4800e9b549566
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2025-04-20 04:24:33 +0200

    xmax basics

--- x_max.py
+++ x_max.py
@@ -2,4 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import utils
 
-print("Hello")
+maxima = np.arange(1, 7)
+maxima_probs = []
+
+for i in range(1, 7):
+    prob = (i ** 2 - ((i - 1) ** 2)) / 36
+    maxima_probs.append(prob)
+
+print(maxima_probs)
+print(sum(maxima_probs))
