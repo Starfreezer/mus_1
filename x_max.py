fc27a1785e1819cc6cec71abc51056b23698fdea
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2025-04-20 04:31:41 +0200

    xmax done

--- x_max.py
+++ x_max.py
@@ -10,4 +10,22 @@ for i in range(1, 7):
     maxima_probs.append(prob)
 
 print(maxima_probs)
-print(sum(maxima_probs))
+
+discrete = utils.discrete_distribution(maxima_probs)
+print(discrete)
+
+plt, ax = plt.subplots(2, 1)
+
+ax[0].bar(maxima, maxima_probs)
+ax[0].set(xlim=(0, 7), xticks=np.arange(0, 7), ylim=(0, 0.5))
+ax[0].set_xlabel("Maximum")
+ax[0].set_ylabel("P(Max(x1,x2) = i)")
+
+ax[1].step(maxima, discrete)
+ax[1].set(xlim=(0, 7), xticks=np.arange(0, 7), ylim=(0, 1.2))
+ax[1].set_xlabel("t")
+ax[1].set_ylabel("P(Max(x1,x2) <= t")
+
+
+plt.tight_layout()
+plt.show()
